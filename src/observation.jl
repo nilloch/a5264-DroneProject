557e8b8260c513d42b174f6b2c444fadbffc8f02
Luke Roberson <luke.roberson@colorado.edu>
2024-05-01 17:01:37 -0600

    working???????????

--- src/observation.jl
+++ src/observation.jl
@@ -25,42 +25,23 @@ function Z(entity)
         return [0.25, 0.25, 0.5, 0.0]
     end
 end
-@show perm(ind_obs,3)
+@show obs = perm(ind_obs,3)
+obs2idx_dict = Dict((obs[i]) => i for i in 1:length(obs))
+idx2obs_dict = Dict((i) => obs[i] for i in 1:length(obs))
 @show probs = (Z(:T) * Z(:B)') .* reshape(Z(:D), 1, 1, :)
 @show probs = vec(probs)
-
-
-
-
-
-
-# const OBS_DIRS = SVector(DSPos(0,1),
-#                         DSPos(1,0),
-#                         DSPos(0,-1),
-#                         DSPos(-1,0),
-#                         DSPos(0,0),
-#                         DSPos(1,1),
-#                         DSPos(1,-1),
-#                         DSPos(-1,-1),
-#                         DSPos(-1,1))
 
 const OBS_QUAD = [:SW, :NW, :NE, :SE, :DET, :OUT]
 const N_OBS_PERFECT = 10
 const N_OBS_QUAD = 6
 
-POMDPs.observations(pomdp::DroneSurveillancePOMDP{QuadCam}) = 1:N_OBS_QUAD
+POMDPs.observations(pomdp::DroneSurveillancePOMDP{QuadCam}) = 1:length(obs)
 POMDPs.observations(pomdp::DroneSurveillancePOMDP{PerfectCam}) = 1:N_OBS_PERFECT
 POMDPs.obsindex(pomdp::DroneSurveillancePOMDP, o::Int64) = o
 
 function POMDPs.observation(pomdp::DroneSurveillancePOMDP{QuadCam}, a::Int64, s::DSState)
-    obs = SVector{N_OBS_QUAD}(1:N_OBS_QUAD)
-    probs = zeros(MVector{N_OBS_QUAD})
-    obs_dir = pomdp.target - s.quad 
-    obs_ind = findfirst(isequal(obs_dir), OBS_DIRS)
 
-    probs[1] = 1.0
-    # @show obs
-    return SparseCat(obs, probs)
+    return SparseCat(1:length(obs), probs)
 
     # if obs_ind == nothing 
     #     probs[6] = 1.0
@@ -91,16 +72,16 @@ function POMDPs.observation(pomdp::DroneSurveillancePOMDP{QuadCam}, a::Int64, s:
     # end
 end
 
-function POMDPs.observation(pomdp::DroneSurveillancePOMDP{PerfectCam}, a::Int64, s::DSState)
-    obs = SVector{N_OBS_PERFECT}(1:N_OBS_PERFECT)
-    probs = zeros(MVector{N_OBS_PERFECT})
-    obs_dir = pomdp.target-s.quad 
-    obs_ind = findfirst(isequal(obs_dir), OBS_DIRS)
-    if obs_ind == nothing 
-        probs[10] = 1.0
-        return SparseCat(obs, probs)
-    else
-        probs[obs_ind] = 1.0
-        return SparseCat(obs, probs)
-    end
-end
+# function POMDPs.observation(pomdp::DroneSurveillancePOMDP{PerfectCam}, a::Int64, s::DSState)
+#     obs = SVector{N_OBS_PERFECT}(1:N_OBS_PERFECT)
+#     probs = zeros(MVector{N_OBS_PERFECT})
+#     obs_dir = pomdp.target-s.quad 
+#     obs_ind = findfirst(isequal(obs_dir), OBS_DIRS)
+#     if obs_ind == nothing 
+#         probs[10] = 1.0
+#         return SparseCat(obs, probs)
+#     else
+#         probs[obs_ind] = 1.0
+#         return SparseCat(obs, probs)
+#     end
+# end
